b37e9f9495a0534b48112f8753f74e4fe1739d3a
Quoc Hoang <quochoang0207@gmail.com>
2020-07-10 15:40:00 +0700

    done

--- game.js
+++ game.js
@@ -12,18 +12,21 @@ We also load all of our images.
 
 let canvas;
 let ctx;
+let score = 0
+let gameOver = false
 
 canvas = document.createElement("canvas");
 ctx = canvas.getContext("2d");
-canvas.width = 512;
-canvas.height = 480;
+canvas.width = 750;
+canvas.height = 650;
 document.body.appendChild(canvas);
 
-let bgReady, heroReady, monsterReady;
-let bgImage, heroImage, monsterImage;
+let bgReady, heroReady, monsterReady, lazerReady;
+let bgImage, heroImage, monsterImage, lazerImage;
+
 
 let startTime = Date.now();
-const SECONDS_PER_ROUND = 30;
+const SECONDS_PER_ROUND = 1000;
 let elapsedTime = 0;
 
 function loadImages() {
@@ -32,13 +35,13 @@ function loadImages() {
     // show the background image
     bgReady = true;
   };
-  bgImage.src = "images/background.png";
+  bgImage.src = "images/backgroundblack.png"
   heroImage = new Image();
   heroImage.onload = function () {
     // show the hero image
     heroReady = true;
   };
-  heroImage.src = "images/hero.png";
+  heroImage.src = "images/spaceship.png";
 
   monsterImage = new Image();
   monsterImage.onload = function () {
@@ -46,6 +49,14 @@ function loadImages() {
     monsterReady = true;
   };
   monsterImage.src = "images/monster.png";
+
+  lazerImage = new Image();
+  lazerImage.onload = function () {
+    // show the hero image
+    lazerReady = true;
+  };
+  lazerImage.src = "images/lazer.png"
+
 }
 
 /** 
@@ -58,11 +69,14 @@ function loadImages() {
  * The same applies to the monster.
  */
 
-let heroX = canvas.width / 2;
-let heroY = canvas.height / 2;
+let heroX = 350
+let heroY = 550
 
-let monsterX = 100;
+let monsterX = 250;
 let monsterY = 100;
+
+let lazerX = 400
+let lazerY = 400
 
 /** 
  * Keyboard Listeners
@@ -91,36 +105,75 @@ function setupKeyboardListeners() {
  *  If you change the value of 5, the player will move at a different rate.
  */
 let update = function () {
+  console.log(heroX, " lazer ", lazerX,);
+
   // Update the time.
+
+  if ((SECONDS_PER_ROUND - elapsedTime) <= 0) {
+    gameOver = true
+    return
+  }
+
   elapsedTime = Math.floor((Date.now() - startTime) / 1000);
 
-
-  if (38 in keysDown) { // Player is holding up key
-    heroY -= 5;
-  }
-  if (40 in keysDown) { // Player is holding down key
-    heroY += 5;
-  }
+  // if (38 in keysDown) { // Player is holding up key
+  //   heroY -= 5;
+  // }
+  // if (40 in keysDown) { // Player is holding down key
+  //   heroY += 5;
+  // }
   if (37 in keysDown) { // Player is holding left key
-    heroX -= 5;
+    heroX -= 10;
   }
   if (39 in keysDown) { // Player is holding right key
-    heroX += 5;
+    heroX += 10;
   }
+  lazerY -= 30;
 
   // Check if player and monster collided. Our images
   // are about 32 pixels big.
+  // if (
+  //   // lazerX <= (monsterX + 32)
+  //   // && monsterX <= (lazerX + 5)
+  //    lazerY <= (monsterY + 32)
+  //    && monsterY <= (lazerY + 20)
+  // ) {
+  //   // Pick a new location for the monster.
+  //   // Note: Change this to place the monster at a new, random location.
+  //   monsterX = Math.random() * canvas.width - 32;
+  //   monsterY = Math.random() * 400;
+  //   // score++
+  // }
   if (
-    heroX <= (monsterX + 32)
-    && monsterX <= (heroX + 32)
-    && heroY <= (monsterY + 32)
-    && monsterY <= (heroY + 32)
+    heroX <= monsterX
+    && monsterX <= heroX
+    && lazerY <= monsterY
+    && monsterY <= lazerY
   ) {
-    // Pick a new location for the monster.
-    // Note: Change this to place the monster at a new, random location.
-    monsterX = monsterX + 50;
-    monsterY = monsterY + 70;
+    monsterX = Math.random() * canvas.width - 32;
+    monsterY = Math.random() * 400;
   }
+
+  if (heroX > canvas.width - 80) {
+    heroX = canvas.width - 80;
+  }
+  if (heroX < 0) {
+    heroX = 0;
+  }
+
+  if (lazerY >= canvas.height) {
+    lazerY = 550;
+  }
+  if (lazerY < 0) {
+    lazerY = canvas.height + 550
+  }
+
+  // if(heroY >= canvas.height - 32){
+  //   heroY = 0;
+  // }
+  // if(heroY < 0){
+  //   heroY = canvas.height-32
+  // }
 };
 
 /**
@@ -133,10 +186,20 @@ var render = function () {
   if (heroReady) {
     ctx.drawImage(heroImage, heroX, heroY);
   }
+
+  if (lazerReady) {
+    ctx.drawImage(lazerImage, heroX + 46, lazerY);
+  }
   if (monsterReady) {
     ctx.drawImage(monsterImage, monsterX, monsterY);
   }
+  if (gameOver == true) {
+    ctx.fillText(`GAME OVER!  `, 220, 250);
+  }
   ctx.fillText(`Seconds Remaining: ${SECONDS_PER_ROUND - elapsedTime}`, 20, 100);
+  ctx.fillText(`Score:${score}`, 20, 120);
+
+  ctx.font = "18px Arial";
 };
 
 /**
@@ -145,12 +208,14 @@ var render = function () {
  * render (based on the state of our game, draw the right things)
  */
 var main = function () {
-  update(); 
+  update();
   render();
   // Request to do this again ASAP. This is a special method
   // for web browsers. 
   requestAnimationFrame(main);
 };
+
+
 
 // Cross-browser support for requestAnimationFrame.
 // Safely ignore this line. It's mostly here for people with old web browsers.
